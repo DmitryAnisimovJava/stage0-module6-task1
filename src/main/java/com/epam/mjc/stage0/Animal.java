3e906857375c8f5a2ec55c22f2a671e6c8fb3ede
d.anisimov <d.anisimov@ds-manager03.szspspb.local>
2023-05-03 19:05:09 +0300

    Module 6. Task 1 done.

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -2,4 +2,19 @@ package com.epam.mjc.stage0;
 
 public class Animal {
 
+	private String color;
+	private int numberOfPaws;
+	private boolean hasFur;
+	
+	public Animal(String color, int numberOfPaws, boolean hasFur) {
+		this.color = color;
+		this.numberOfPaws = numberOfPaws;
+		this.hasFur = hasFur;
+	}
+	
+	public String getDescription() {
+		String hasFurAnswerString = hasFur == true ? "a" : "no";
+		String pawOrPaws = numberOfPaws <= 1 ? "paw" : "paws";
+		return "This animal is mostly %s. It has %s %s and %s fur.".formatted(color, numberOfPaws, pawOrPaws, hasFurAnswerString);
+	}
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,13 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
-
+public class Bird extends Animal{
+	
+	public Bird() {
+		super("blue", 2, false);
+	}
+	@Override 
+	public String getDescription() {
+		String animalResultString = super.getDescription();
+		return animalResultString + " Moreover, it has 2 wings and can fly.";
+	}
 }
