7271fbfc651106312595c3f30dac8b34718cdfc2
d.anisimov <d.anisimov@ds-manager03.szspspb.local>
2023-05-03 19:06:36 +0300

    fixed.

--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -5,10 +5,4 @@ public class Dog extends Animal{
 	public Dog() {
 		super("brown", 4, true);
 	}
-	public static void main(String[] args) {
-		Dog myDog = new Dog();
-		Bird myBird = new Bird();
-		System.out.println(myDog.getDescription());
-		System.out.println(myBird.getDescription());
-	}
 }
